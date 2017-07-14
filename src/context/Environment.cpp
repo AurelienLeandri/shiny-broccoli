5602df459fb7c6ab73ed5cfbdb5e56a430f6a963
AurelienLeandri <aurelien.leandri@gmail.com>
2017-07-14 16:54:36 +0200

    Add base class for environment and agents

---
+++ src/context/Environment.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leo on 7/14/17.
+//
+
+#include "Environment.hpp"
---
+++ src/context/Environment.hpp
@@ -0,0 +1,13 @@
+//
+// Created by leo on 7/14/17.
+//
+
+#pragma once
+
+
+class Environment {
+  public:
+    ~Environment() = 0;
+};
+
+
