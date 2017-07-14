99f8f3feaa7b67f4c74671c8a096c058c815b668
AurelienLeandri <aurelien.leandri@gmail.com>
2017-07-14 13:03:03 +0200

    Add test and sample main files

---
+++ samples/game/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  // FIXME: Starting point of the sample game
+  return 0;
+}
---
+++ test/misc/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  // FIXME: General testing, unitary tests for functions etc
+  return 0;
+}
