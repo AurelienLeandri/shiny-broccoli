04d0b2e863b424602a9b58ab1a4724a1c95778fc
AurelienLeandri <aurelien.leandri@gmail.com>
2017-07-25 15:46:27 +0200

    Make modify function const

--- src/context/environment.hpp
+++ src/context/environment.hpp
@@ -16,9 +16,9 @@ namespace broccoli {
 
     public:
       template <typename T>
-      void modify(T &data, void (*operation)(T &));
+      void modify(T &data, void (*operation)(T &)) const;
       template <typename T1, typename T2>
-      void modify(T1 &data1, T2 data2, void (*operation)(T1 &, T2));
+      void modify(T1 &data1, T2 data2, void (*operation)(T1 &, T2)) const;
       void set_data_manager(DataManager *data_manager) { _data_manager = data_manager; }
 
 
@@ -28,12 +28,12 @@ namespace broccoli {
   };
 
   template<typename T>
-  void Environment::modify(T &data, void (*operation)(T &)) {
+  void Environment::modify(T &data, void (*operation)(T &)) const {
     _data_manager->add_action(new DataUpdate<T>(data, operation, *_data_manager));
   }
 
   template<typename T1, typename T2>
-  void Environment::modify(T1 &data1, T2 data2, void (*operation)(T1 &, T2)) {
+  void Environment::modify(T1 &data1, T2 data2, void (*operation)(T1 &, T2)) const {
     _data_manager->add_action(new DataUpdate<T1, T2>(data1, data2, operation, *_data_manager));
   }
 
